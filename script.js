79e75e57474d1d12ffc16d3fedcc2e00ca3d0150
Micael-Macedo <micaelm2009@hotmail.com>
2023-03-08 21:15:50 -0300

    style: add responsive content to mobile

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-document.onload(function () => {
-    $(".YEAR").hide()
+$(document).ready(function () {
+    $(".YEARLY").hide()
 })
 function signature(option){
     $("button").closest(".selected").removeClass("selected");
     $(option).addClass("selected");
     if($(option).html() === "MONTHLY"){
-        $(".YEAR").hide()
+        $(".YEARLY").hide()
         $(".MONTHLY").show()
     }
     if($(option).html() === "YEARLY"){
         $(".MONTHLY").hide()
-        $(".YEAR").show()
+        $(".YEARLY").show()
     }
 }
